28caf5edf993e31eb0525b6aad12894e986cf6fc
skorpion86041 <blackskorpio1st@gmail.com>
2018-06-19 18:24:21 +0300

    menu working!=)

---
+++ training.py
@@ -0,0 +1,64 @@
+# abc = 2+5*2
+#
+# print(abc)
+#
+# abc = 'abc'
+#
+# print(abc)
+
+# a = 100
+# b = 200
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+
+# x = y = 0
+# x += 1
+# print(x)
+# print(y)
+
+# print(13 == 13)
+# print(1 != 2)
+# print(2 == 4)
+# print(3 > 4)
+# print(3 <= 3)
+# print(6 >= 6)
+# print(6 < 5)
+# x = 2
+# print(1 < x < 3)
+
+# print(bool(12))
+# print(bool(0))
+# print(bool(45))
+# print(bool(-10))
+# print(bool(2.9))
+
+# x, y = True, False
+# print(x and y)
+# print(x or y)
+
+# y = False
+# print(not y)
+
+# x = True
+# print(not x)
+
+
+# a, b, c, d = True, False, True, True
+# result = a and b or c
+# print(result)
+# print(d)
+# result = not d
+# print(result)
+
+
+# Задача: определить, високосный год или нет?
+# Год является високосным, если он кратен 4, но при этом не кратен 100, либо кратен 400.
+
+# year = 2018
+# is_leap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+# print(is_leap)
+
+# import calendar
+# print(calendar.isleap(1080))
+
